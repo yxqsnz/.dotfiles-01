1940215c65a475f8b96602b46b1ac9b883d561b5
yxqsnz <yxqsnz@gmail.com>
2022-03-12 16:35:12 -0300

    update init.sh

--- init.sh
+++ init.sh
@@ -2,4 +2,4 @@
 echo "=> Link Neovim"
 test -d ~/.config/nvim || ln -sv $(realpath nvim) ~/.config/nvim
 echo "=> Link git"
-test -d ~/.config/nvim || ln -sv $(realpath .gitconfig) ~ 
+test -d ~/.gitconfig || ln -sv $(realpath .gitconfig) ~ 

fc66e2e307595b4fab75dc8e4eff660967033b53
yxqsnz <yxqsnz@gmail.com>
2022-03-12 16:39:44 -0300

    update init.sh

--- init.sh
+++ init.sh
@@ -2,4 +2,6 @@
 echo "=> Link Neovim"
 test -d ~/.config/nvim || ln -sv $(realpath nvim) ~/.config/nvim
 echo "=> Link git"
-test -d ~/.gitconfig || ln -sv $(realpath .gitconfig) ~ 
+test -d ~/.gitconfig || ln -sv $(realpath .gitconfig) ~
+echo "=> Link kitty"
+test -d ~/.config/kitty || ln -sv $(realpath kitty) ~/.config/kitty 

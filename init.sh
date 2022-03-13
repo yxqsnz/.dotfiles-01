72737671cd95fc9277cace7a15c6250701c5b47e
yxqsnz <yxqsnz@gmail.com>
2022-03-12 22:18:59 -0300

    kde: add kde globals

--- init.sh
+++ init.sh
@@ -5,3 +5,5 @@ echo "=> Link git"
 test -d ~/.gitconfig || ln -sv $(realpath .gitconfig) ~
 echo "=> Link kitty"
 test -d ~/.config/kitty || ln -sv $(realpath kitty) ~/.config/kitty 
+echo "=> Link KDE Globals"
+test -d ~/.config/kdeglobals || ln -sv $(realpath kdeglobals) ~/.config/kdeglobals

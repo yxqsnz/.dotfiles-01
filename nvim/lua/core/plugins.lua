888ab2be0e801d950592ac4344b9d368fa8e40f1
yxqsnz <yxqsnz@gmail.com>
2022-03-13 08:33:08 -0300

    nvim: whoops

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -1,13 +1,13 @@
 local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
---if fn.empty(fn.glob(install_path)) > 0 then
---  Packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
---  vim.cmd[[exit]]
---end
+if fn.empty(fn.glob(install_path)) > 0 then
+  Packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  vim.cmd[[exit]]
+end
 
 local packer = require('packer')
 packer.startup(function (use)
-  --use ({ 'wbthomason/packer.nvim' })
+  use ({ 'wbthomason/packer.nvim' })
   for _, plugin in pairs(OmegaVim.plugins) do
 	  local plug = {
 		  plugin.url,

54445576dc41e3a57ceff597d6b1853815f47e24
yxqsnz <yxqsnz@gmail.com>
2022-03-15 18:20:04 -0300

    add doom emacs config

--- init.sh
+++ init.sh
@@ -3,3 +3,5 @@ echo "=> Link git"
 test -d ~/.gitconfig || ln -sv $(realpath .gitconfig) ~
 echo "=> Link kitty"
 test -d ~/.config/kitty || ln -sv $(realpath kitty) ~/.config/kitty 
+echo "=> Link Doom Emacs"
+test -d ~/.doom.d || ln -sv $(realpath emacs) ~/.doom.d

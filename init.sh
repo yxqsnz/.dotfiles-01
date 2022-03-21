806162cebc4d1bcd5cd2ecf655bc6e55e14fc0cc
yxqsnz <yxqsnz@gmail.com>
2022-03-21 15:45:10 -0300

    add cargo config

--- init.sh
+++ init.sh
@@ -5,3 +5,5 @@ echo "=> Link kitty"
 test -d ~/.config/kitty || ln -sv $(realpath kitty) ~/.config/kitty 
 echo "=> Link Doom Emacs"
 test -d ~/.doom.d || ln -sv $(realpath .doom.d) ~/.doom.d
+echo "=> Link Cargo config"
+test -d ~/.cargo/config.toml || ln -sv $(realpath cargo-config.toml) ~/.cargo/config.toml

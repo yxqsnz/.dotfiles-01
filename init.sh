c65dac02d3763b8270d74de5b0214cebb8eebd53
yxqsnz <yxqsnz@gmail.com>
2022-03-21 15:36:30 -0300

    add doom config

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "=> Link git"
-test -d ~/.gitconfig || ln -sv $(realpath .gitconfig) ~
+test -f ~/.gitconfig || ln -sv $(realpath .gitconfig) ~/.gitconfig
 echo "=> Link kitty"
 test -d ~/.config/kitty || ln -sv $(realpath kitty) ~/.config/kitty 
-echo "=> Link Emacs"
-test -d ~/.emacs.d || ln -sv $(realpath emacs) ~/.emacs.d
+echo "=> Link Doom Emacs"
+test -d ~/.doom.d || ln -sv $(realpath .doom.d) ~/.doom.d

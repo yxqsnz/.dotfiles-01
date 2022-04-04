f98294c41143795f5de97ea9a8d3cb93d53b4550
yxqsnz <yxqsnz@gmail.com>
2022-04-04 20:52:34 -0300

    update

--- link.sh
+++ link.sh
@@ -6,7 +6,6 @@ link() {
 	else 
 		export IS_DIR=false
 	fi
-	
 	check() {
 		if $IS_DIR; then
 			return $(test -d $1)
@@ -21,14 +20,5 @@ link() {
 		echo "[✗] skipped ${SRC}"
 	fi
 }
-
-link  bspwm ~/.config/bspwm -p 
-link  sxhkd ~/.config/sxhkd -p
-link  eww ~/.config/eww     -p 
-link  dunst ~/.config/dunst -p
-link  alacritty ~/.config/alacritty -p 
-link  picom ~/.config/picom -p 
-link  .doom.d ~/.doom.d     -p
-link  .gitconfig ~/.gitconfig 
-link  .zshrc ~/.zshrc
-link  cargo-config.toml ~/.cargo/config.toml
+link home-manager ~/.config/nixpkgs -p
+link cargo-config.toml ~/.cargo/config.toml

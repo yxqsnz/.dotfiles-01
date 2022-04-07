bd9e45e8bff95179647b0fedbf1d835d32114750
yxqsnz <yxqsnz@gmail.com>
2022-04-07 17:27:24 -0300

    update

--- hosts/base.nix
+++ hosts/base.nix
@@ -2,7 +2,9 @@
    boot.loader.systemd-boot.enable = true;
    boot.loader.efi.canTouchEfiVariables = true;
    boot.plymouth.enable = true;
+   programs.dconf.enable = true;
    boot.kernelPackages = pkgs.linuxPackages_xanmod;
+   networking.useDHCP = false;
    networking.networkmanager.enable = true;
    imports = [
      ../users/yxqsnz/default.nix

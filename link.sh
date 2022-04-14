3e7acc37af2baba363d966612b1ef3dc6f2c0c8a
yxqsnz <yxqsnz@gmail.com>
2022-04-14 17:50:26 -0300

    Update link.sh

--- link.sh
+++ link.sh
@@ -2,19 +2,28 @@
 source "std.sh"
 link "etc/doom-emacs" ~/.doom.d
 link "etc/cargo.toml" ~/.cargo/config.toml
-link "etc/gitconfig" ~/.gitconfig
 link "etc/zshrc" ~/.zshrc
 link "etc/nixpkgs" ~/.config/nixpkgs
 link "etc/starship.toml" ~/.config/starship.toml
+ok "Ensuring packages,."
 ensure git
 ensure exa
 ensure zsh
 ensure ccache
 ensure lld
 ensure clang
+ensure curl
+ensure nix
 ok "Running first run setup..."
 zsh -c "source ~/.zshrc && exit"
 ok "Setting Zsh as default shell for $USER."
 chsh -s $(which zsh)
 ok "Ensuring starship..."
 curl -sS https://starship.rs/install.sh | sh -s -- -y
+echo "-[NIX]-> Installing Home Mananger and adding nix unstable repository"
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --add https://nixos.org/channels/nixos-unstable nixpkgs
+nix-channel --update
+echo "-[NIX]-> Installing Home Manager and switching"
+nix-shell '<home-manager>' -A install
+home-manager switch

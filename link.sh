fdff38d8f6f9e528e581591ca0e8072e5e3f477b
yxqsnz <yxqsnz@gmail.com>
2022-04-19 14:35:25 -0300

    update

--- link.sh
+++ link.sh
@@ -3,12 +3,17 @@ source "std.sh"
 link "etc/doom-emacs" ~/.doom.d
 link "etc/cargo.toml" ~/.cargo/config.toml
 link "etc/zshrc" ~/.zshrc
-#link "etc/nixpkgs" ~/.config/nixpkgs
+link "etc/nixpkgs" ~/.config/nixpkgs
 link "etc/starship.toml" ~/.config/starship.toml
+link "etc/bspwm"  ~/.config/
+link "etc/sxhkd"  ~/.config/
+link "etc/dunst"  ~/.config/
+link "etc/polybar" ~/.config/
 ok "Ensuring packages,."
-ensure git exa zsh ccache lld clang curl steam amdvlk vulkan-radeon lib32-vulkan-radeon lib32-amdvlk \
-	   discord-canary firefox telegram-desktop pipewire bat asp ttf-nerd-fonts-symbols rustup \
-	   noto-fonts-cjk noto-fonts-extra ttf-joypixels lldb gdb github-cli
+#ensure git exa zsh ccache lld clang curl steam amdvlk vulkan-radeon lib32-vulkan-radeon lib32-amdvlk \
+#	   discord-canary firefox telegram-desktop pipewire bat asp ttf-nerd-fonts-symbols rustup \
+#	   noto-fonts-cjk noto-fonts-extra ttf-joypixels lldb gdb github-cli
+ensure $(cat pkgs.list)
 ok "Running first run setup..."
 zsh -c "source ~/.zshrc && exit"
 ok "Setting Zsh as default shell for $USER."

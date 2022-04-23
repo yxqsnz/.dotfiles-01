a4e163667e041fe38ed4dc8aa57abf8efbe7b367
yxqsnz <yxqsnz@gmail.com>
2022-04-23 11:23:38 -0300

    std: add a option to remove current link

--- std.sh
+++ std.sh
@@ -9,11 +9,18 @@ err() {
 }
 
 link() {
-  unlink $2
+  if [[ "${FORCE_UNLINK}" == '1' ]]; then
+    unlink $2 2>/dev/null
+  fi
   if ln -s $(realpath $1) $2 2>/dev/null; then
     ok "linked $1 into $2."
   else
     err "failed to link $1."
+    printf "unlink directory? "
+    if read -q; then
+      unlink $2 2>/dev/null
+    fi
+
   fi
 }
 ensure() {

88aa98ad5edce48d7109e07835b7db20b3e9fc91
yxqsnz <yxqsnz@gmail.com>
2022-07-13 21:02:54 -0300

    update

--- configs/nvim/lua/core/packerInit.lua
+++ configs/nvim/lua/core/packerInit.lua
@@ -298,6 +298,31 @@ return packer.startup(function(use)
     end,
   })
 
+  use({
+    "Pocco81/AutoSave.nvim",
+    config = function()
+      local autosave = require("autosave")
+
+      autosave.setup({
+        enabled = true,
+        execution_message = "AutoSave: saved at " .. vim.fn.strftime(
+          "%H:%M:%S"
+        ),
+        events = { "InsertLeave", "TextChanged" },
+        conditions = {
+          exists = true,
+          filename_is_not = {},
+          filetype_is_not = {},
+          modifiable = true,
+        },
+        write_all_buffers = false,
+        on_off_commands = true,
+        clean_command_line_interval = 0,
+        debounce_delay = 135,
+      })
+    end,
+  })
+
   use({ "~/.vim/plugins/yex-lang/", opt = true })
   use({ "andweeb/presence.nvim", event = "VimEnter" })
   use({ "github/copilot.vim" })

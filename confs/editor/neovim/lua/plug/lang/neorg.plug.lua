e9803d8bd9e41c595177d04840cb21461222ddae
yxqsnz <yxqsnz@gmail.com>
2022-09-07 20:44:06 -0300

    feat(neorg): styling change

--- confs/editor/neovim/lua/plug/lang/neorg.plug.lua
+++ confs/editor/neovim/lua/plug/lang/neorg.plug.lua
@@ -1,15 +1,31 @@
 plugin {
   'nvim-neorg/neorg',
   ft = 'norg',
+  cmd = 'Neorg',
   after = 'nvim-treesitter',
-
   config = function()
     require('neorg').setup {
       load = {
         ['core.defaults'] = {},
+        ['external.kanban'] = {},
+        ['core.gtd.base'] = {
+          config = {
+            workspace = 'default',
+          },
+        },
+        ['core.norg.concealer'] = {
+          config = {
+            icon_preset = 'diamond',
+          },
+        },
+        ['core.norg.completion'] = {
+          config = {
+            engine = 'nvim-cmp',
+          },
+        },
       },
     }
   end,
 
-  requires = 'nvim-lua/plenary.nvim',
+  requires = { 'nvim-lua/plenary.nvim', 'max397574/neorg-kanban' },
 }
